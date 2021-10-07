91b180c5d7e40236ad517118d5f79a2f1b181bec
shruthishruu05 <shruthi.m@perfios.com>
2021-10-07 20:08:48 +0530

    REFACTORED : validation for space between the pin code

--- PinCodeValidation.js
+++ PinCodeValidation.js
@@ -18,3 +18,10 @@ pinRegex = RegExp('^[0-9]{6}$');
 arguments.forEach(element => {
     console.log("UC3 :" +element+": ", pinRegex.test(element));
 });
+
+pinRegex = RegExp("(^[0-9]{3})([\s]?)([0-9]{3}$)");
+
+arguments.forEach(element => {
+    console.log("UC4 :" +element+": ", pinRegex.test(element));
+});
+
